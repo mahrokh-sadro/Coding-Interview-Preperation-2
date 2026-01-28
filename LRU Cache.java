dcd8171c1e4ed9aa8cecbbd910be14186fa48860
mahrokh sadro <mahrokh.sadro@j-sas.com>
2026-01-28 16:14:27 -0500

    practice

---
+++ LRU Cache.java
@@ -0,0 +1,113 @@
+// Design a data structure that
+// follows the constraints of a
+// Least Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize
+// the LRU cache with positive size
+// capacity.
+// int get(int key) Return the value 
+// of the key if the key exists,
+// otherwise return -1.
+// void put(int key, int value) 
+// Update the value of the key if
+// the key exists. Otherwise, add
+// the key-value pair to the cache.
+// If the number of keys exceeds the
+// capacity from this operation, evict
+// the least recently used key.
+// The functions get and put must each
+// run in O(1) average time complexity.
+
+// 1 <= capacity <= 3000
+// 0 <= key <= 10^4
+// 0 <= value <= 10^5
+// At most 2 * 10^5 calls will
+// be made to get and put.
+
+class LRUCache {
+
+    private class Node{
+        int value;
+        int key;
+        Node pre;
+        Node next;
+        public Node(){
+
+        }
+        public Node(int key,int value){
+            this.key=key;
+            this.value=value;
+        }
+
+    }
+    
+    Map<Integer,Node> map;
+    Node head;
+    Node tail;
+    int capacity=0;
+    public LRUCache(int capacity) {
+        map=new HashMap<>();
+        this.capacity=capacity;
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head.next=tail;
+        tail.pre=head;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)){
+           return -1;
+        }
+        Node node=map.get(key);
+        remove(node);
+        addToHead(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+            node.value=value;
+            remove(node);
+            addToHead(node);
+        }
+        else{
+           Node newNode=new Node(key,value);
+           addToHead(newNode);
+           map.put(key,newNode);
+        }
+
+        if(map.size()>capacity){
+            // Node lru=map.get(key);
+            Node lru=tail.pre;
+            remove(lru);
+            map.remove(lru.key);
+        }
+
+    }
+
+    private void remove(Node node){
+       node.pre.next=node.next;
+       node.next.pre=node.pre; 
+    }
+
+    private void addToHead(Node node){
+       node.next=head.next;
+       head.next.pre=node;
+       node.pre=head;
+       head.next=node;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+
+
+
---
+++ Swap Nodes in Pairs.java
@@ -0,0 +1,49 @@
+// Given a linked list, swap every two
+// adjacent nodes and return its head.
+// You must solve the problem without
+// modifying the values in the list's
+// nodes (i.e., only nodes themselves
+// may be changed.)
+
+// The number of nodes in the list is
+// in the range [0, 100].
+// 0 <= Node.val <= 100
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        ListNode tail=head;
+        for(int i=0;i<2;i++){
+            if(tail==null) return head;
+            tail=tail.next;
+        }
+        
+        ListNode newHead=reverse( head, tail);
+        head.next=swapPairs(tail);
+        return newHead;
+
+    }
+
+    private ListNode reverse(ListNode head,ListNode tail){
+        ListNode cur=head;
+        ListNode pre=null;
+        while(cur!=tail){
+            ListNode next=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=next;
+        }
+        return pre;
+    }
+}
+// Time:O(n)
+// Space:O(n) (recursion stack)
